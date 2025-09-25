f5fe3856d11e59fed232c4c1e01e4d7e0614d704
scott <sszalay>
2025-09-25 01:55:54 -0400

    added parse apache logs

--- week4/main.ps1
+++ week4/main.ps1
@@ -1,3 +1,7 @@
 ﻿. .\apache_logs_9.ps1
 
 Get-ApacheIPs 
+
+. .\parse_apache_logs.ps1
+
+ApacheLogs1
---
+++ week4/parse_apache_logs.ps1
@@ -0,0 +1,22 @@
+﻿function ApacheLogs1() {
+    $logsNotformatted = Get-Content "C:\xampp\apache\logs\access.log"
+    $tableRecords = @()
+
+    for ($i=0; $i -lt $logsNotformatted.Count; $i++) {
+        # split a string into words
+        $words = $logsNotformatted[$i].Split(" ")
+
+        $tableRecords += [PSCustomObject]@{
+            "IP"       = $words[0]
+            "Time"     = $words[3].Trim('[', ']')
+            "Method"   = $words[5].Trim('"')
+            "Page"     = $words[6]
+            "Protocol" = $words[7]
+            "Response" = $words[8]
+            "Referrer" = $words[10]
+            "Client"   = $words[11..($words.Count - 1)] -join ' '
+        }
+    }
+
+    return $tableRecords | Where-Object { $_.IP -like "10.*" }
+}
