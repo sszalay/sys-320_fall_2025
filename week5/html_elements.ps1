d1ac03001541031d2dfc6f7959d6ce972303156b
scott <sszalay>
2025-10-15 14:15:24 -0400

    html elements ps1 code

---
+++ week5/html_elements.ps1
@@ -0,0 +1,12 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.26/ToBeScraped.html
+
+$scraped_page.Links | Select-Object outerText, href, innerText
+
+$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2")
+$h2s
+
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {
+    $_.getAttributeNode("class").Value -ilike "*div-1*"
+} | select innerText
+
+$divs1
